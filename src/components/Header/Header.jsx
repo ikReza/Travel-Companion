cdf262f3bcaa8cffe819036cc578ee74aa9cea6a
ikReza <ireza.kaiser00@gmail.com>
2022-04-05 19:22:04 +0600

    setup header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,26 @@
+import { Autocomplete } from "@react-google-maps/api";
+import { AppBar, Toolbar, Typography, InputBase, Box } from "@mui/material";
+import { Search } from "@mui/icons-material";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h5">Travel Advisor</Typography>
+        <Box display="flex">
+          <Typography variant="h6">Explore new places</Typography>
+        </Box>
+        {/* <Autocomplete> */}
+        <div>
+          <div>
+            <Search />
+          </div>
+          <InputBase placeholder="Search..." />
+        </div>
+        {/* </Autocomplete> */}
+      </Toolbar>
+    </AppBar>
+  );
 };
 
 export default Header;
